c01cc54933e74a29384ac22bdf4dd022213d076a
Ryan Dy <rdy@pivotal.io>
2016-08-27 01:01:28 -0700

    improve promise version of flat map to not pause but instead queue the promise to be ended, do not check instanceOf Promise use then instead

--- src/flat-map.js
+++ src/flat-map.js
@@ -3,39 +3,37 @@ const through = require('through');
 const Promise = require('any-promise');
 
 function isPromise(obj) {
-  return obj instanceof Promise;
+  return obj.then;
 }
 
 function flatten(data) {
-  if (Array.isArray(data)) return data.forEach(this.queue);
-  if (isStream(data)) return new Promise((resolve, reject) => {
-    let err;
-    data.on('error', e => (err = e, reject(e))).pipe(through(this.queue, () => {
-      if (err) return reject(err);
-      resolve();
-    }));
-  });
-  this.queue(data);
+  if (isStream(data)) {
+    return new Promise((resolve, reject) => {
+      let err;
+      data.on('error', e => (err = e, reject(e))).pipe(through(this.queue, () => {
+        if (err) return reject(err);
+        resolve(data);
+      }));
+    });
+  }
+  if (Array.isArray(data)) {
+    data.forEach(this.queue);
+  } else {
+    this.queue(data);
+  }
+  return data;
 }
 
 function flatMap(callback) {
   const promises = [];
   let index = 0;
 
-  function pushPromise(data) {
-    const result = this::flatten(data);
-    if (isPromise(result)) promises.push(result);
-  }
-
   return through(function(data) {
     callback(data, (err, data) => {
       if (err) this.emit('error', err);
-
-      if (isPromise(data)) {
-        this.pause();
-        return data.then(this::pushPromise, err => this.emit('error', err)).then(this.resume, this.resume);
-      }
-      this::pushPromise(data);
+      if (isPromise(data)) return promises.push(data.then(this::flatten, err => this.emit('error', err)));
+      const result = this::flatten(data);
+      if (isPromise(result)) promises.push(result);
     }, index++);
   }, function() {
     const end = () => this.queue(null);
